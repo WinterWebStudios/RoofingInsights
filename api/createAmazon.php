192ec542a6771b724cd7c55bf522525335b088b6
Celtech <timhinz16@gmail.com>
2018-01-14 03:46:59 -0600

    Restructer

--- api/createAmazon.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require_once("settings.php");
-    $image = $_POST['image'];
-    $text = $_POST['text'];
-    $connection->query("INSERT INTO `amazon` (`id`, `image`, `text`) VALUES (NULL, '$image', '$text')");
-    header( 'Location: ../admin/admin.php' ) ;
--- api/settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("DEBUG",0);
-define("LIVE",1);
-
-$connection = new mysqli("localhost","root","","roofinginsights");
-$version = DEBUG;
-
-session_start();
