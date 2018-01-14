95bf14f7d65270e022d4f4630fd97e4975f35359
Celtech <timhinz16@gmail.com>
2018-01-14 04:58:30 -0600

    removed map

--- source/api/settings.php
+++ source/api/settings.php
@@ -1,8 +1,11 @@
 <?php
 define("DEBUG",0);
 define("LIVE",1);
-
-$connection = new mysqli("localhost","root","","roofinginsights");
+if( in_array( $_SERVER['REMOTE_ADDR'], array( '127.0.0.1', '::1' ) ) ) {
+    $connection = new mysqli("localhost", "root", "", "roofinginsights");
+}else{
+    $connection = new mysqli("localhost", "root", "dimond23", "roofinginsights");
+}
 $version = DEBUG;
 
 session_start();
